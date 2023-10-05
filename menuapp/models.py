13711f4f447c9d493e40a29af9e6fa6afb55b100
Evgeniy K <pgg1337@mail.ru>
2023-10-05 14:07:44 +0300

    add model MenuItem

--- menuapp/models.py
+++ menuapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=50)
+    parent = models.ForeignKey(to='self', on_delete=models.CASCADE, related_name='children', null=True, blank=True)
+    url = models.CharField(max_length=128)
+    is_active = models.BooleanField(default=False)
+    menu_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
