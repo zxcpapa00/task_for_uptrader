818324905f27c2b0d9ebea1d604d5b4adfd51a08
Evgeniy K <pgg1337@mail.ru>
2023-10-07 12:52:18 +0300

    add test url

--- task_for_uptrader/urls.py
+++ task_for_uptrader/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from menuapp.views import menu_view, about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('menu/<str:menu_name>', menu_view),
 ]
