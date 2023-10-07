3f92e305a3d98becdcbeb27ff89b893f33e512eb
Evgeniy K <pgg1337@mail.ru>
2023-10-07 12:50:32 +0300

    add test view

--- menuapp/views.py
+++ menuapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import MenuItem
+from django.template import RequestContext
 
-# Create your views here.
+
+def menu_view(request, menu_name):
+    return render(request, 'menu.html')
+
+
+def about_view(request, menu_name='main_menu'):
+    return render(request, 'about.html')
